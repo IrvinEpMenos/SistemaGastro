2b40201c4b5f0f53aeb67ca81a7775df11f4f335
21307019 <21307019@utcgg.edu.mx>
2024-05-23 19:04:13 +0000

    Agregar datatables

--- views/registrarEncargado.php
+++ views/registrarEncargado.php
@@ -4,21 +4,20 @@
 <div class="mx-auto pt-5" style="width: 65%"  >
 <form class="row g-3">
   <div class="col-md-4">
-    <label for="validationDefault01" class="form-label">First name</label>
+    <label for="validationDefault01" class="form-label">Nombres</label>
     <input type="text" class="form-control" id="validationDefault01" value="Mark" required>
   </div>
   <div class="col-md-4">
-    <label for="validationDefault02" class="form-label">Last name</label>
+    <label for="validationDefault02" class="form-label">Apellidos</label>
     <input type="text" class="form-control" id="validationDefault02" value="Otto" required>
   </div>
   <div class="col-md-4">
-    <label for="validationDefaultUsername" class="form-label">Username</label>
+    <label for="validationDefaultUsername" class="form-label">Matricula</label>
     <div class="input-group">
-      <span class="input-group-text" id="inputGroupPrepend2">@</span>
-      <input type="text" class="form-control" id="validationDefaultUsername" aria-describedby="inputGroupPrepend2" required>
+      <input type="number" maxlength="8" minlength="5" class="form-control" id="validationDefaultUsername" aria-describedby="inputGroupPrepend2" required>
     </div>
   </div>
-  <div class="col-md-6">
+<div class="col-md-6">
     <label for="validationDefault03" class="form-label">City</label>
     <input type="text" class="form-control" id="validationDefault03" required>
   </div>
